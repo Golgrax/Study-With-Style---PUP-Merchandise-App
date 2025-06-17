0303cbb808c5f90b919bc3b61751a97cf0ebb534
Karl Benjamin Bughaw <pepe.kotelurow@gmail.com>
2025-06-17 02:29:28 +0000

    fixed back to inventory button position in home screen 	modified:   KIVY APP/kv/home_screen.kv 	modified:   KIVY APP/screens/home_screen.py

--- KIVY APP/screens/home_screen.py
+++ KIVY APP/screens/home_screen.py
@@ -10,35 +10,28 @@ class HomeScreen(Screen):
     def on_pre_enter(self, *args):
         app = App.get_running_app()
         
-        order_history_btn = self.ids.get('order_history_button')
-        contact_us_btn = self.ids.get('contact_us_button')
-        back_to_inventory_btn = self.ids.get('back_to_inventory_button')
+        user_nav = self.ids.get('user_nav')
+        admin_nav = self.ids.get('admin_nav')
 
         if app.is_admin:
-            order_history_btn.opacity = 0
-            order_history_btn.size_hint_x = 0
-            order_history_btn.disabled = True
-            
-            contact_us_btn.opacity = 0
-            contact_us_btn.size_hint_x = 0
-            contact_us_btn.disabled = True
-            
-            back_to_inventory_btn.opacity = 1
-            back_to_inventory_btn.size_hint_x = 1
-            back_to_inventory_btn.disabled = False
+            if user_nav:
+                user_nav.height = 0
+                user_nav.opacity = 0
+                user_nav.disabled = True
+            if admin_nav:
+                admin_nav.height = '50dp'
+                admin_nav.opacity = 1
+                admin_nav.disabled = False
         else:
-            order_history_btn.opacity = 1
-            order_history_btn.size_hint_x = 1
-            order_history_btn.disabled = False
-            
-            contact_us_btn.opacity = 1
-            contact_us_btn.size_hint_x = 1
-            contact_us_btn.disabled = False
-            
-            back_to_inventory_btn.opacity = 0
-            back_to_inventory_btn.size_hint_x = 0
-            back_to_inventory_btn.disabled = True
-             
+            if user_nav:
+                user_nav.height = '50dp'
+                user_nav.opacity = 1
+                user_nav.disabled = False
+            if admin_nav:
+                admin_nav.height = 0
+                admin_nav.opacity = 0
+                admin_nav.disabled = True
+        
         best_seller = self.db_manager.fetch_best_seller()
         default_img = os.path.join(os.path.dirname(__file__), '..', '..', 'pup_study_style', 'static', 'assets', 'question_mark.png')
         
@@ -47,7 +40,10 @@ class HomeScreen(Screen):
             best_seller_label = self.ids.get('best_seller_label')
             if best_seller_image:
                 img_path = best_seller['image_path'] if 'image_path' in best_seller.keys() else None
-                best_seller_image.source = img_path if img_path and os.path.exists(img_path.replace('../', '')) else default_img
+                if img_path and os.path.exists(str(img_path).replace('../', '')):
+                    best_seller_image.source = img_path
+                else:
+                    best_seller_image.source = default_img
             if best_seller_label:
                 best_seller_label.text = best_seller['name']
         
@@ -64,10 +60,14 @@ class HomeScreen(Screen):
         
         for product in other_products:
             img_path = product['image_path'] if 'image_path' in product.keys() else None
+            if img_path and os.path.exists(str(img_path).replace('../', '')):
+                source = img_path
+            else:
+                source = default_img
             item = ProductItem(
                 product_id=product['id'],
                 product_name=product['name'],
                 product_price=f"Price: P{product['price']:.2f}",
-                image_source=img_path if img_path and os.path.exists(img_path.replace('../', '')) else default_img
+                image_source=source
             )
             product_list.add_widget(item)
