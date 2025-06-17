7ced9606d9a78c9c8017ab2e23bbed08a9b6f7a7
Karl Benjamin Bughaw <pepe.kotelurow@gmail.com>
2025-06-17 01:30:41 +0000

    THANK YOU SO MUCH MICO!!! 	modified:   KIVY APP/create_profiles_table.sql

--- KIVY APP/create_profiles_table.sql
+++ KIVY APP/create_profiles_table.sql
@@ -81,4 +81,4 @@ CREATE TABLE IF NOT EXISTS users (
     username TEXT UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
     is_admin BOOLEAN NOT NULL DEFAULT 0
-);
+); -- lagyan pa ba talaga info sa users table? parang redundant na kasi may profiles table na, so di ko na nilagyan ng address and contact info dito
